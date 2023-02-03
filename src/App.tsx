995682d8f4ba488d20064ee46b6506b4ec934fb9
Sangwon Park <wkqkel@naver.com>
2023-02-04 04:52:39 +0900

    FEAT: image test

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ export default function App({name}: {name: string}) {
 	return (
 		<div>
 			<Greeting name='greeting' />
+			<img src='/images/test.png' alt='test' width={200}/>
 			<p>Hello, {name}</p>
 		</div>
 	);
