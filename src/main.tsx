e8e200e757526aff57503d1898d58eb1bf49f8c6
BORA <70184893+rara-record@users.noreply.github.com>
2023-02-03 18:28:29 +0900

    feat: 설정 파일 추가

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return <p>개발 환경 만들기</p>;
+}
+
+const container = document.getElementById('root');
+ReactDOM.render(<App />, container);
