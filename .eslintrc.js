7ca3dfa78e1a202d1b93199751decebf8ade0180
yodoree <info@hbmedi.kr>
2023-02-03 20:53:03 +0900

    1주차 과제

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,56 @@
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-		jest: true,
-	},
-	extends: ['plugin:react/recommended', 'xo'],
-	overrides: [
-		{
-			extends: ['xo-typescript'],
-			files: ['*.ts', '*.tsx'],
-		},
-	],
-	parserOptions: {
-		ecmaVersion: 'latest',
-		sourceType: 'module',
-	},
-	plugins: ['react'],
-	rules: {},
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: ['plugin:react/recommended', 'xo', 'plugin:react/jsx-', 'xo'],
+  overrides: [
+    {
+      extends: ['xo-typescript'],
+      files: ['*.ts', '*.tsx'],
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'import/extensions': 'never',
+    'no-console': 'warn',
+    'no-plusplus': 'off',
+    'no-shadow': 'off',
+    'vars-on-top': 'off',
+    'no-underscore-dangle': 'off', // var _foo;
+    'comma-dangle': 'off',
+    'func-names': 'off', // setTimeout(function () {}, 0);
+    'prefer-template': 'off',
+    'no-nested-ternary': 'off',
+    'max-classes-per-file': 'off',
+    'consistent-return': 'off',
+    'no-restricted-syntax': ['off', 'ForOfStatement'], // disallow specified syntax(ex. WithStatement)
+    'prefer-arrow-callback': 'error', // Require using arrow functions for callbacks
+    'require-await': 'error',
+    'arrow-parens': ['error', 'as-needed'], // a => {}
+    'no-param-reassign': ['error', { props: false }],
+    'no-unused-expressions': [
+      'error',
+      {
+        allowTernary: true, // a || b
+        allowShortCircuit: true, // a ? b : 0
+        allowTaggedTemplates: true,
+      },
+    ],
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'max-len': [
+      'error',
+      {
+        code: 120,
+        ignoreComments: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+      },
+    ],
+  },
 };
---
+++ src/components/Greeting.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from '@testing-library/react';
+
+import Greeting from './Greeting';
+
+test('Greeting', () => {
+  render(<Greeting name="world" />);
+
+  screen.getByText('Hello, world');
+
+  screen.getByText(/Hello/);
+
+  expect(screen.queryByText(/Hello/)).toBeInTheDocument();
+
+  expect(screen.queryByText(/Hi/)).not.toBeInTheDocument();
+
+  expect(screen.queryByText(/Hi/)).not.toBeVisible();
+
+  expect(screen.queryByText(/Hi/)).toBeFalsy();
+});
