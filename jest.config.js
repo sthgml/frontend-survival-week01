fd9e0f3802b9bb88ee31f270c497086a2dac227f
dlxotjde <dlxotjde@daum.net>
2023-02-05 23:24:13 +0900

    remove jest setup

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,7 @@
 module.exports = {
 	testEnvironment: 'jsdom',
 	setupFilesAfterEnv: [
-		'@testing-library/jest-dom/extend-expect', // 는 사실 설치를 위에서 하긴 함
-		'./jest.setup', // 이건 안쓴다 지워
+		'@testing-library/jest-dom/extend-expect',
 	],
 	transform: {
 		'^.+\\.(t|j)sx?$': ['@swc/jest', {
