9baa79b5d77fc8f2b0b77c529bc80852ad89484f
taeck <taeck007@gmail.com>
2023-02-08 17:38:25 +0900

    코드 컨벤션 수정

--- src/components/Greeting.test.tsx
+++ src/components/Greeting.test.tsx
@@ -2,13 +2,13 @@ import { render, screen } from "@testing-library/react";
 import Greeting from "./Greeting";
 
 test("Greeting", () => {
-  render(<Greeting name="world!" />);
+	render(<Greeting name="world!" />);
 
-  screen.getByText("Hello, world!");
+	screen.getByText("Hello, world!");
 
-  screen.getByText(/Hello/);
+	screen.getByText(/Hello/);
 
-  expect(screen.queryByText(/Hi/)).toBeFalsy();
+	expect(screen.queryByText(/Hi/)).toBeFalsy();
 
-  expect(screen.queryByText(/Hi/)).not.toBeInTheDocument();
+	expect(screen.queryByText(/Hi/)).not.toBeInTheDocument();
 });
