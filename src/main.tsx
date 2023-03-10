83f3cae467fd367100729b3fdc63f42db39feff5
ShinjungOh <tlswjd9260@gmail.com>
2023-03-10 23:53:22 +0900

    chore: index.html, main.tsx 파일 기본 세팅 추가

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import ReactDOM from 'react-dom/client';
+
+function App() {
+	return (
+		<p>Hello world</p>
+	);
+}
+
+const element = document.getElementById('root');
+
+if (element) {
+	const root = ReactDOM.createRoot(element);
+
+	root.render(<App/>);
+}
+
