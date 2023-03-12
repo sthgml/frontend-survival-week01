662427bb505864c1dcc5a41093b4c7c6411c0ef5
jungjinbeom <insunok0715@naver.com>
2023-03-12 21:05:02 +0900

    eslint 적용

--- src/components/Greeting.tsx
+++ src/components/Greeting.tsx
@@ -1,3 +1,3 @@
-export default function Greeting({ name }: { name: string }) {
-  return <p> Hello, {name}!</p>;
+export default function Greeting({name}: {name: string}) {
+	return <p> Hello, {name}!</p>;
 }
