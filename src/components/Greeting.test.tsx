35e30943120556221f632633205817526596405c
shiny1912 <shiny1912@naver.com>
2023-03-12 20:50:35 +0900

    미포함 파일들 추가

---
+++ src/components/Greeting.test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react';
+
+import Greeting from './Greeting';
+
+test('Greeting', () => {
+    render(<Greeting name='World' />);
+
+    screen.getByText('Hello, World!');
+
+    screen.getByText(/Hello/);
+
+    expect(screen.queryByText(/Hi/)).not.toBeInTheDocument();
+});
---
+++ src/main.test.tsx
@@ -0,0 +1,18 @@
+function add(x: number, y: number): number {
+    return x + y;
+}
+
+const context = describe;
+
+describe('add 함수는', () => {
+    it('두 숫자의 합을 돌려준다', () => {
+        expect(add(1, 2)).toBe(3);
+    });
+
+    context('두 개의 양수가 주어졌을 때', () => {
+        it('항상 두개의 숫자보다 큰 값을 돌려준다', () => {
+            expect(add(1, 2)).toBeGreaterThan(1);
+            expect(add(1, 2)).toBeGreaterThan(2);
+        });
+    });
+});
