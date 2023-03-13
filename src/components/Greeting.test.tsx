606aa79574f87224857cee3b94ea9ccac7229972
StarryPro <hansunguk6178@gmail.com>
2023-03-13 11:02:31 +0900

    fix: Greeting test 코드 수정

--- src/components/Greeting.test.tsx
+++ src/components/Greeting.test.tsx
@@ -4,8 +4,6 @@ import Greeting from './Greeting';
 test('Greeting', () => {
   render(<Greeting name='world' />);
 
-  screen.getByText('Hello, world!');
-
   screen.getByText(/Hello/);
 
   expect(screen.queryByText(/Hello/)).toBeInTheDocument();
