784d420a923862c1241893ddef9f4192ec8a41da
oyathb <130042529+oyathb@users.noreply.github.com>
2023-04-18 19:37:41 +0900

    Delete src directory

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function App({name}: {
-	name: string;
-}) {
-	return (
-		<p>HELLO {name}!</p>
-	);
-}
--- src/main.test.tsx
+++
@@ -1,7 +0,0 @@
-function add(x: number, y: number): number {
-	return x + y;
-}
-
-test('add 함수 테스트', () => {
-	expect(add(1, 2)).toBe(3);
-});
