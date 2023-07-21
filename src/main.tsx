d4e2ff836a0e090e6e7c5959e3db7c2301f89488
함우성 <wooseongham@gmail.com>
2023-07-22 01:57:08 +0900

    lint 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
 function main() {
-  const container = document.getElementById("root");
-  if (!container) {
-    return;
-  }
+	const container = document.getElementById('root');
+	if (!container) {
+		return;
+	}
 
-  const root = ReactDOM.createRoot(container);
-  root.render(<App />);
+	const root = ReactDOM.createRoot(container);
+	root.render(<App />);
 }
 
 main();
