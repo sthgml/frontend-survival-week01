19d11b441b249d4a1c0eb1374e7298f1600a8e03
FEWoobIn <hwb0218@gmail.com>
2023-07-21 21:26:57 +0900

    Feat: App 컴포넌트 이미지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import {useState} from 'react';
 import Greeting from './components/Greeting';
 
 type AppProps = {
@@ -5,9 +6,20 @@ type AppProps = {
 };
 
 export default function App({name}: AppProps) {
+	const [count, setCount] = useState(0);
+
+	const handleClick = () => {
+		setCount(count + 1);
+	};
+
 	return (
 		<div>
-			<Greeting name={'world!'}/>
+			<Greeting name='world' />
+			<img src='/images/terminal-node.png' alt='terminal noden' width='200' />
+			<p>Count: {count}</p>
+			<button type='button' onClick={handleClick}>
+        클릭!
+			</button>
 		</div>
 	);
 }
