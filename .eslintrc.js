43be8d3da858c93286b495c089d801064d82d1d1
bereal1995 <gygud98@gmail.com>
2023-07-23 05:15:19 +0900

    feat: README 내용대로 설정 추가

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+		jest: true,
+	},
+	extends: [
+		'plugin:react/recommended',
+		'plugin:react/jsx-runtime',
+		'xo',
+	],
+	overrides: [
+		{
+			env: {
+				node: true,
+			},
+			files: [
+				'.eslintrc.{js,cjs}',
+			],
+			parserOptions: {
+				sourceType: 'script',
+			},
+		},
+		{
+			extends: [
+				'xo-typescript',
+			],
+			files: [
+				'*.ts',
+				'*.tsx',
+			],
+		},
+	],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: [
+		'react',
+	],
+	rules: {
+	},
+};
---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import ReactDOM from 'react-dom/client';
+
+import App from './App';
+
+function main() {
+	const element = document.getElementById('root');
+
+	if (!element) {
+		return;
+	}
+
+	const root = ReactDOM.createRoot(element);
+
+	root.render(<App />);
+}
+
+main();
