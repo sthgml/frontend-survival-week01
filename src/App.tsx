9512ec9d17bb988d92545eaa694caf4f8a99fd23
Jaehun <qkdwogns98@gmail.com>
2023-07-23 13:56:49 +0900

    요구사항-2 대한 내용 수정.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import Greeting from './components/Greeting';
 
 const App = () => (
-	<div>App 컴포넌트입니다. 화면에 보이나요?</div>
+	<div>
+		<Greeting name='jaehun'/>
+	</div>
 );
 
 export default App;
