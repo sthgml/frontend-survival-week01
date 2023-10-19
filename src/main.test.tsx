a7a4cbf919c3fc7340db0c65bdc946cb2bcf58ba
uhuikim <uhui94@naver.com>
2023-10-19 21:53:41 +0900

    fix : 테스트 파일 추가 및 오타 수정

---
+++ src/main.test.tsx
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+
+import Greeting from './components/Greeting';
+
+test('Greeting', () => {
+    render(<Greeting name="world" />);
+    screen.getByText(/Hello world/);
+});
