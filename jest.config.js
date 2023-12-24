9ae0a394383a5321c58d7cb0bb69423e47b53008
박소희 <sthgml@naver.com>
2023-12-24 17:29:20 +0900

    eslint 5

--- jest.config.js
+++ jest.config.js
@@ -1,30 +1,30 @@
 // In jest.config.js add (if you haven't already)
 
 module.exports = {
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: [
-    '@testing-library/jest-dom/extend-expect',
-    './jest.setup',
-    '<rootDir>/jest-setup.js',
-  ],
-  transform: {
-    '^.+\\.(t|j)sx?$': ['@swc/jest', {
-      jsc: {
-        parser: {
-          syntax: 'typescript',
-          jsx: true,
-          decorators: true,
-        },
-        transform: {
-          react: {
-            runtime: 'automatic',
-          },
-        },
-      },
-    }],
-  },
-  testPathIgnorePatterns: [
-    '<rootDir>/node_modules/',
-    '<rootDir>/dist/',
-  ],
+	testEnvironment: 'jsdom',
+	setupFilesAfterEnv: [
+		'@testing-library/jest-dom/extend-expect',
+		'./jest.setup',
+		'<rootDir>/jest-setup.js',
+	],
+	transform: {
+		'^.+\\.(t|j)sx?$': ['@swc/jest', {
+			jsc: {
+				parser: {
+					syntax: 'typescript',
+					jsx: true,
+					decorators: true,
+				},
+				transform: {
+					react: {
+						runtime: 'automatic',
+					},
+				},
+			},
+		}],
+	},
+	testPathIgnorePatterns: [
+		'<rootDir>/node_modules/',
+		'<rootDir>/dist/',
+	],
 };
