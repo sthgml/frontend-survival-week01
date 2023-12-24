83118ffc374b890b2242f2dac144589991b9b8ca
박소희 <sthgml@naver.com>
2023-12-24 16:17:04 +0900

    설정파일

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,10 @@ module.exports = {
         "es2021": true,
         "jest": true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:react/recommended",
-        "plugin:react/jsx-runtime",
-        'xo',
+    extends: [
+        'airbnb',
+        'plugin:react/recommended',
+        'plugin:react/jsx-runtime',
     ],
     "overrides": [
         {
@@ -33,6 +31,50 @@ module.exports = {
         "@typescript-eslint",
         "react"
     ],
-    "rules": {
+    settings: {
+        'import/resolver': {
+            node: {
+            extensions: ['.js', '.jsx', '.ts', '.tsx'],
+            },
+        },
+    },
+    rules: {
+        indent: ['error', 2],
+        'no-trailing-spaces': 'error',
+        curly: 'error',
+        'brace-style': 'error',
+        'no-multi-spaces': 'error',
+        'space-infix-ops': 'error',
+        'space-unary-ops': 'error',
+        'no-whitespace-before-property': 'error',
+        'func-call-spacing': 'error',
+        'space-before-blocks': 'error',
+        'keyword-spacing': ['error', { before: true, after: true }],
+        'comma-spacing': ['error', { before: false, after: true }],
+        'comma-style': ['error', 'last'],
+        'comma-dangle': ['error', 'always-multiline'],
+        'space-in-parens': ['error', 'never'],
+        'block-spacing': 'error',
+        'array-bracket-spacing': ['error', 'never'],
+        'object-curly-spacing': ['error', 'always'],
+        'key-spacing': ['error', { mode: 'strict' }],
+        'arrow-spacing': ['error', { before: true, after: true }],
+        'import/no-extraneous-dependencies': ['error', {
+            devDependencies: [
+                '**/*.test.js',
+                '**/*.test.jsx',
+                '**/*.test.ts',
+                '**/*.test.tsx',
+            ],
+        }],
+        'import/extensions': ['error', 'ignorePackages', {
+            js: 'never',
+            jsx: 'never',
+            ts: 'never',
+            tsx: 'never',
+        }],
+        'react/jsx-filename-extension': [2, {
+            extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        }],
     },
 }
